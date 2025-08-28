58e37a13d049494a51164db6aa18e84d86699275
0xardy <ardy@wonderland.xyz>
2025-08-28 12:23:09 -0300

    wonderland/feat: add usdc in testnet

--- src/consts/pinnedTokens.ts
+++ src/consts/pinnedTokens.ts
@@ -33,5 +33,20 @@ export const PINNED_TOKENS: PinnedTokens = [
     chainId: 10n,
     address: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85',
     onGasTank: true
+  },
+  {
+    chainId: 11155111n,
+    address: '0x0000000000000000000000000000000000000000',
+    onGasTank: false
+  },
+  {
+    chainId: 11155111n,
+    address: '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238',
+    onGasTank: false
+  },
+  {
+    chainId: 421614n,
+    address: '0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d',
+    onGasTank: false
   }
 ]
