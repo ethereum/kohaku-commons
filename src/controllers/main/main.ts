71cd9bfd316d9adfd9bb1a1a008d37e922609e3f
Onizuka-wl <onizuka@wonderland.xyz>
2025-09-02 12:18:13 -0300

    feat: 🎸 pass dependencies down

--- src/controllers/main/main.ts
+++ src/controllers/main/main.ts
@@ -78,6 +78,7 @@ import { RequestsController } from '../requests/requests'
 import { SelectedAccountController } from '../selectedAccount/selectedAccount'
 import {
   SIGN_ACCOUNT_OP_MAIN,
+  SIGN_ACCOUNT_OP_PRIVACY_POOLS,
   SIGN_ACCOUNT_OP_SWAP,
   SIGN_ACCOUNT_OP_TRANSFER,
   SignAccountOpType
@@ -497,7 +498,19 @@ export class MainController extends EventEmitter {
       }
     })
 
-    this.privacyPools = new PrivacyPoolsController(this.keystore, privacyPoolsAspUrl, alchemyApiKey)
+    this.privacyPools = new PrivacyPoolsController(
+      this.keystore,
+      privacyPoolsAspUrl,
+      alchemyApiKey,
+      this.accounts,
+      this.networks,
+      this.providers,
+      this.selectedAccount,
+      this.portfolio,
+      this.activity,
+      this.#externalSignerControllers,
+      relayerUrl
+    )
   }
 
   /**
@@ -733,10 +746,14 @@ export class MainController extends EventEmitter {
 
     let signAccountOp: SignAccountOpController | null
 
+    console.log('DEBUG: handleSignAndBroadcastAccountOp', type)
+
     if (type === SIGN_ACCOUNT_OP_MAIN) {
       signAccountOp = this.signAccountOp
     } else if (type === SIGN_ACCOUNT_OP_SWAP) {
       signAccountOp = this.swapAndBridge.signAccountOpController
+    } else if (type === SIGN_ACCOUNT_OP_PRIVACY_POOLS) {
+      signAccountOp = this.privacyPools.signAccountOpController
     } else {
       signAccountOp = this.transfer.signAccountOpController
     }
@@ -2007,6 +2024,14 @@ export class MainController extends EventEmitter {
       }
 
       this.transfer.resetForm()
+    }
+
+    if (type === SIGN_ACCOUNT_OP_PRIVACY_POOLS) {
+      if (this.privacyPools.shouldTrackLatestBroadcastedAccountOp) {
+        this.privacyPools.latestBroadcastedAccountOp = submittedAccountOp
+      }
+
+      this.privacyPools.resetForm()
     }
 
     await this.#notificationManager.create({
