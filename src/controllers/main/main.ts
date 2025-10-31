7cf8846992c8410f79b4cfde5585bc4214a85b9d
Onizuka-wl <onizuka@wonderland.xyz>
2025-10-31 12:52:50 -0300

    fix: change ambire to kohaku

--- src/controllers/main/main.ts
+++ src/controllers/main/main.ts
@@ -778,7 +778,7 @@ export class MainController extends EventEmitter {
       if (!wasAlreadySigned) {
         if (!signAccountOp) {
           const message =
-            'The signing process was not initialized as expected. Please try again later or contact Ambire support if the issue persists.'
+            'The signing process was not initialized as expected. Please try again later or contact Kohaku support if the issue persists.'
 
           throw new EmittableError({ level: 'major', message })
         }
@@ -1028,7 +1028,7 @@ export class MainController extends EventEmitter {
       !accountAddr || !chainId || !this.accounts.accountStates?.[accountAddr]?.[chainId.toString()]
     if (isAccountStateStillMissing) {
       const message =
-        'Unable to sign the message. During the preparation step, required account data failed to get received. Please try again later or contact Ambire support.'
+        'Unable to sign the message. During the preparation step, required account data failed to get received. Please try again later or contact Kohaku support.'
       const error = new Error(
         `The account state of ${accountAddr} is missing for the network with id ${chainId}.`
       )
@@ -1061,7 +1061,7 @@ export class MainController extends EventEmitter {
       const ledgerCtrl = this.#externalSignerControllers.ledger
       if (!ledgerCtrl) {
         const message =
-          'Could not initialize connection with your Ledger device. Please try again later or contact Ambire support.'
+          'Could not initialize connection with your Ledger device. Please try again later or contact Kohaku support.'
         throw new EmittableError({ message, level: 'major', error: new Error(message) })
       }
 
@@ -1111,7 +1111,7 @@ export class MainController extends EventEmitter {
 
       if (!trezorCtrl) {
         const message =
-          'Could not initialize connection with your Trezor device. Please try again later or contact Ambire support.'
+          'Could not initialize connection with your Trezor device. Please try again later or contact Kohaku support.'
         throw new EmittableError({ message, level: 'major', error: new Error(message) })
       }
 
@@ -1144,7 +1144,7 @@ export class MainController extends EventEmitter {
       const latticeCtrl = this.#externalSignerControllers.lattice
       if (!latticeCtrl) {
         const message =
-          'Could not initialize connection with your Lattice1 device. Please try again later or contact Ambire support.'
+          'Could not initialize connection with your Lattice1 device. Please try again later or contact Kohaku support.'
         throw new EmittableError({ message, level: 'major', error: new Error(message) })
       }
 
