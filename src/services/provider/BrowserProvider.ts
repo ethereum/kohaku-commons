8b6aa7df3e6d84f84fdb7fadee742dd1d096a76e
Karen Sarkisyan <karen.sarkisyan01@gmail.com>
2025-11-06 16:02:29 +0100

    Opt out of Helios for transactions broadcasting

--- src/services/provider/BrowserProvider.ts
+++ src/services/provider/BrowserProvider.ts
@@ -1,14 +1,24 @@
 /* eslint-disable no-underscore-dangle */
 
-import { BrowserProvider as BrowserProviderEthers, Eip1193Provider, EnsResolver } from 'ethers'
+import { BrowserProvider as BrowserProviderEthers, EnsResolver, JsonRpcProvider } from 'ethers'
 import { RPCProvider } from '../../interfaces/provider'
+import { HeliosEthersProvider } from './HeliosEthersProvider'
 
 export class BrowserProvider extends BrowserProviderEthers implements RPCProvider {
+  #heliosProvider: HeliosEthersProvider
+
   readonly rpcUrl: string
 
-  constructor(provider: Eip1193Provider, rpcUrl: string) {
+  constructor(provider: HeliosEthersProvider, rpcUrl: string) {
     super(provider)
+    this.#heliosProvider = provider
     this.rpcUrl = rpcUrl
+  }
+
+  // @TODO Now that Helios abstraction is leaking all over the place,
+  // we should rework provider structure and move fallback logic away from Helios provider
+  getFallbackProvider(): JsonRpcProvider {
+    return this.#heliosProvider.getFallbackProvider()
   }
 
   _getConnection(): { url: string } {
