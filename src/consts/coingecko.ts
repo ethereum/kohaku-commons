cfc3d1efaf7ba9ef88d1d9646fc968b738b51050
Olaleye Blessing <Olayinkablexxy@gmail.com>
2026-03-03 10:04:04 +0100

    feat: Map stablecoins to CoinGecko iDs

--- src/consts/coingecko.ts
+++ src/consts/coingecko.ts
@@ -13,7 +13,13 @@ export function geckoIdMapper(address: string, network: Network): string | null 
   // we currently can't map aave so we're leaving this
   if (address === '0x4da27a545c0c5B758a6BA100e3a049001de870f5') return 'aave'
 
-  return null
+  const STABLE_GECKO_IDS: Record<string, string> = {
+    '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238': 'usd-coin', // Sepolia
+    '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': 'usd-coin' // Mainnet
+    // add others as needed
+  }
+
+  return STABLE_GECKO_IDS[address] ?? null
 }
 
 /**
